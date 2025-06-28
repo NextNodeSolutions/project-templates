5a5304080de7a74662cf410933750515cffc9ccd
walid-mos <pro.walid.mostefaoui@gmail.com>
2025-06-28 11:44:12 +0200

    feat: add astro template

---
+++ apps/astro/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
+import node from '@astrojs/node'
+import react from '@astrojs/react'
+
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 4321
+const site = process.env.URL || `http://${host}:${port}`
+
+console.log({ port, site, host })
+
+// https://astro.build/config
+export default defineConfig({
+	site,
+	server: {
+		port,
+		host,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	adapter: node({
+		mode: 'standalone',
+	}),
+	integrations: [react()],
+})
